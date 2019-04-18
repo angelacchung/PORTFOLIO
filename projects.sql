e5cff23e4ad15afc14b5a9ebf3afe10e22a3c6b3
angelacchung <44277537+angelacchung@users.noreply.github.com>
2019-04-17 22:58:45 -0400

    sdfsdf

---
+++ projects.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 17, 2019 at 10:54 PM
+-- Server version: 5.5.61-cll
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chunmini_portfolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE `projects` (
+  `id` int(50) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `photo` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `tag` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`id`, `title`, `photo`, `description`, `tag`) VALUES
+(1, 'CustomHighze', 'proj-1.png', 'There\'s over 700 types of known cannabis strains in the world. If you\'re a potential new user (or just plain curious), a number like that can be intimidating. Find your way through this field with a starting point.\r\n', 'UX/UI'),
+(2, 'Domino\'s Pizza UX case study', 'proj-2.jpg', 'An analysis of online delivery between two pizzerias; marking pain points and areas in which improvements can be made.   ', 'UX/UI ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
