1c75aa6f60b2c36902d835cbc75d8d88e190f993
angelacchung <44277537+angelacchung@users.noreply.github.com>
2019-04-21 12:29:06 -0400

    ertertert

--- js/main.js
+++ js/main.js
@@ -5,3 +5,20 @@ $("#mailMe").click(function(){
     $("#contactForm").show();
     $("#mailMe").hide();
 })
+
+// MENU
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos < currentScrollPos) {
+    // $("#navbar").slideUp();
+    document.getElementById("navbar").style.top = "-50%";
+
+  } else {
+    // $("#navbar").slideDown();
+
+    document.getElementById("navbar").style.top = "0";
+
+  }
+  prevScrollpos = currentScrollPos;
+}
